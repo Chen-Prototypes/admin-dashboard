4c300d7a19dc8fbc6a5df9362901a47827d07378
Roger Chen <rogerchen803@gmail.com>
2023-06-27 10:31:17 -0600

    Add better spacing for JS file

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ let project = document.querySelector('.etch-a-sketch');
 project.addEventListener('click', () => {
     window.location.href = 'https://github.com/Chen-Prototypes/etch-a-sketch';
 });
+
 project = document.querySelector('.rock-paper-scissors');
 project.addEventListener('click', () => {
     window.location.href = 'https://github.com/Chen-Prototypes/rock-paper-scissors';
 });
+
 project = document.querySelector('.sign-up');
 project.addEventListener('click', () => {
     window.location.href = 'https://github.com/Chen-Prototypes/sign-up-form';
 });
+
 project = document.querySelector('.landing-page');
 project.addEventListener('click', () => {
     window.location.href = 'https://github.com/Chen-Prototypes/choya-landing-page';
 });
+
 project = document.querySelector('.calculator');
 project.addEventListener('click', () => {
     window.location.href = 'https://github.com/Chen-Prototypes/basic-calculator';
 });
+
 project = document.querySelector('.recipes');
 project.addEventListener('click', () => {
     window.location.href = 'https://github.com/Chen-Prototypes/odin-recipes';
